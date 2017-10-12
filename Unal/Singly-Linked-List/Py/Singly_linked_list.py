e0fe4e51ae01355917bd1ccd564ff38b41db7561
sasalx <oyundanben@gmail.com>
2017-10-12 16:04:09 +0300

    Created the list and insert imp.

---
+++ Unal/Singly-Linked-List/Py/Singly_linked_list.py
@@ -0,0 +1,54 @@
+# Data and next node storage
+
+class Node(object):
+
+    def __init__(self, data = None, next_node = None):
+        self.data = data
+        self.next_node = next_node
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+    def __repr__(self): 
+         return self.data
+
+# Linked list
+
+class linkedList(object):
+
+    def  __init__(self, data_iterable):
+        self.head = None # Start point
+        for data in data_iterable: 
+            self.insert(data)
+
+    # Inserting "from start" // O(1)
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+        
+    def __repr__(self):
+        node = self.head
+        data = []
+        while node:
+            data.append(node.data)
+            node = node.next_node
+        return ' '.join(data)
+
+# Calls
+
+ll = linkedList([""]) # Creation of Linked List
+
+# Insert
+ll.insert("1")
+ll.insert("2")
+ll.insert("3")
+ll.insert("4")
+
+print(ll)
