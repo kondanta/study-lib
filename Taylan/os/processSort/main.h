cf835cbeedf16ca811d2c048edf87671e32b9a3b
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-02-15 14:27:43 +0300

    Os labs

---
+++ Taylan/os/processSort/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#define SIZE 5
+
+
+
+/*Prototypes.*/
+void priorityScheduler(int process[], int burstTime[], int priority[]);
+void roundrobin(int process[], int arrival[], int burstTime []);
+
+
+
+
+#endif //MAIN_H_
