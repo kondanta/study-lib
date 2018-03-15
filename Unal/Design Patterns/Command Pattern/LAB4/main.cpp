bb83b142a203ffb54c5f0951f6222c8e8381201c
sasalx <oyundanben@gmail.com>
2018-03-15 19:15:06 +0300

    Rework in main logic

--- Unal/Design Patterns/Command Pattern/LAB4/main.cpp
+++ Unal/Design Patterns/Command Pattern/LAB4/main.cpp
@@ -56,6 +56,68 @@ class User {
 public:
     void Work(EventHandler *handler) {
         EventHandler *sEvent = nullptr;
+        cout << "Please do the click(Press enter)"<<endl;
+        cin.ignore();
+        _events.push_back(handler);
+        if(_events.size() == 5){
+            for (int i = 0; i < 5; i++) {
+                sEvent = _events[i];
+                sEvent->Click();
+                cout << &_events[i]<<endl;
+            }
+            cout << "Done";
+        }
+    }
+private:
+    vector<EventHandler *>	_events;
+};
+
+// Main
+
+int main(){
+    srand(static_cast<unsigned int>(time(nullptr))); // For random seed gen.
+
+    EventHandler *event1 = nullptr;
+    EventHandler *event2 = nullptr;
+    EventHandler *event3 = nullptr;
+    EventHandler *event4 = nullptr;
+    EventHandler *event5 = nullptr;
+
+    auto *user = new User();
+    auto *mDriver = new MouseDriver();
+
+    event1 = new MouseEventHandler(mDriver);
+    event2 = new MouseEventHandler(mDriver);
+    event3 = new MouseEventHandler(mDriver);
+    event4 = new MouseEventHandler(mDriver);
+    event5 = new MouseEventHandler(mDriver);
+
+    user->Work(event1);
+    user->Work(event2);
+    user->Work(event3);
+    user->Work(event4);
+    user->Work(event5);
+
+    // Dtor
+    delete event1;
+    delete event2;
+    delete event3;
+    delete event4;
+    delete event5;
+    delete user;
+    delete mDriver;
+
+
+}
+
+
+// Another version
+/*
+
+class User {
+public:
+    void Work(EventHandler *handler) {
+        EventHandler *sEvent = nullptr;
         while(!status){
             cout << "Please do the click(Press enter)"<<endl;
             cin.ignore();
@@ -65,6 +127,7 @@ public:
                 for (int i = 0; i < 5; i++) {
                     sEvent = _events[i];
                     sEvent->Click();
+                    cout << &_events[i]<<endl;
                 }
                 cout << "Done";
             }
@@ -81,11 +144,15 @@ int main(){
     srand(static_cast<unsigned int>(time(nullptr))); // For random seed gen.
 
     EventHandler *event = nullptr;
+    EventHandler *event2 = nullptr;
     auto *user = new User();
     auto *mDriver = new MouseDriver();
 
     event = new MouseEventHandler(mDriver);
     user->Work(event);
+    event2 = new MouseEventHandler(mDriver);
+    user->Work(event2);
+
 
     // Dtor
     delete event;
@@ -95,3 +162,4 @@ int main(){
 
 }
 
+*/
