d749e0e5f7cf36f5809585ffdabc4663d2db9d8f
sasalx <oyundanben@gmail.com>
2018-03-28 17:26:30 +0300

    Lowercase/Uppercase Beautify

--- Unal/DOS_Assembly/Lowercase-Uppercase/test.asm
+++ Unal/DOS_Assembly/Lowercase-Uppercase/test.asm
@@ -4,11 +4,13 @@ STACK_SEG SEGMENT STACK USE16
 DB 100 DUP(?)
 STACK_SEG ENDS
 DATA_SEG SEGMENT 'DATA' USE16
+
 MESSAGE DB 'Enter the msg: $',0DH,0AH
 OUTMSG1 DB 0DH,0AH,'Lowercase $'
 OUTMSG2 DB 0DH,0AH,'Uppercase $'
 INSTRING DB 60, ?, 60 DUP('$')
 NEWLINE DB 0DH,0AH,'$'
+
 DATA_SEG ENDS
 CODE_SEG SEGMENT PARA 'CODE' PUBLIC USE16
 ASSUME CS:CODE_SEG, DS:DATA_SEG, SS:STACK_SEG
@@ -19,29 +21,29 @@ PUSH AX
 MOV AX,DATA_SEG
 MOV DS,AX
 
-LEA DX,MESSAGE ;PRINT MESSAGE
-MOV AH,9
-INT 21H
+lea dx,MESSAGE ;PRINT MESSAGE
+mov ah,9
+int 21H
 
-MOV AH,0AH ;READ A string
-LEA DX, INSTRING
-INT 21H
+mov AH,0AH ;READ A string
+lea dx, INSTRING
+int 21H
 
 mov cx,0 ;upc counter
 mov bx,0 ;lwc counter
 
-lea DI, INSTRING+2 ;Put the str to DI
+lea di, INSTRING+2 ;Put the str to DI
 
 loopp:
-mov al, [DI]
+mov al, [di]
 cmp al, 'A'
 jb lowercase ; Below A
 cmp al, 'Z' 
 ja lowercase ; Upper than Z
 
-LEA DX,OUTMSG2 ;inc cx sim.
-MOV AH,9
-INT 21H
+lea dx,OUTMSG2 ;inc cx sim.
+mov AH,9
+int 21H
 
 inc di
 loop loopp
@@ -51,9 +53,9 @@ jb spacecase ; Below a
 cmp al, 'z'
 ja spacecase ; Upper than A
 
-LEA DX,OUTMSG1 ;inc bx sim.
-MOV AH,9
-INT 21H
+lea DX,OUTMSG1 ;inc bx sim.
+mov AH,9
+int 21H
 
 inc di
 loop loopp
@@ -64,6 +66,7 @@ cmp al, ' '
 ja ext ; if not space
 inc di
 loop loopp
+
 ext:
 MOV DX,OFFSET NEWLINE
 MOV AH,9 ;MOVE CURSOR TO NEXT LINE
