c2f2a30e675f93b7b560ff1997bc54c67aff9b5f
Taylan Dogan <doganntaylan@gmail.com>
2018-11-17 00:03:46 +0300

    Seperate decleration and implementation

--- Taylan/dsa/Singly-Linked-List/cpp/sll.cpp
+++ Taylan/dsa/Singly-Linked-List/cpp/sll.cpp
@@ -1,10 +1,48 @@
-#include <math.h>
- 
-double squareRoot(const double a) {
-    double b = sqrt(a);
-    if(b != b) { // nan check
-        return -1.0;
-    }else{
-        return sqrt(a);
-    }
+// Some stuff
+
+template<typename T>
+class Node
+{
+private:
+  T _value;
+  Node *_next;
+public:
+  Node();
+  void setValue(T);
+  T getValue();
+  Node *getNext();
+  void setNext(Node *);
+  bool hasNext();
+  virtual ~Node();
+};
+
+template<typename T>
+Node<T>::Node() = default;
+
+template<typename T>
+Node<T>::~Node() = default;
+
+template<typename T>
+void Node<T>::setValue(T value){
+  this->_value = value;
+}
+
+template<typename T>
+T Node<T>::getValue(){
+  return this->_value;
+}
+
+template<typename T>
+void Node<T>::setNext(Node<T> *next){
+  this->_next = next;
+}
+
+template<typename T>
+Node<T> *Node<T>::getNext(){
+  return this->_next;
+}
+
+template<typename T>
+bool Node<T>::hasNext(){
+  return this->_next == nullptr ? false : true; 
 }
--- Taylan/dsa/Singly-Linked-List/cpp/test.cpp
+++ Taylan/dsa/Singly-Linked-List/cpp/test.cpp
@@ -1,17 +1,32 @@
 // tests.cpp
 #include "sll.cpp"
 #include <gtest/gtest.h>
- 
-TEST(SquareRootTest, PositiveNos) { 
-    ASSERT_EQ(6, squareRoot(36.0));
-    ASSERT_EQ(18.0, squareRoot(324.0));
-    ASSERT_EQ(25.4, squareRoot(645.16));
-    ASSERT_EQ(0, squareRoot(0.0));
+
+// TestCaseName, TestName
+// ASSERT_EQ(6, squareRoot(36.0));
+//TEST(SquareRootTest, PositiveNos) { 
+//}
+class NodeTest : public ::testing::Test {
+protected:
+  // void SetUp() override {
+  //   // some logic
+  // }
+  Node<int> _obj;
+};
+
+TEST_F(NodeTest, IsNullInitially){
+  EXPECT_EQ(nullptr, _obj.getNext());
 }
- 
-TEST(SquareRootTest, NegativeNos) {
-    ASSERT_EQ(-1.0, squareRoot(-15.0));
-    ASSERT_EQ(-1.0, squareRoot(-0.2));
+
+TEST_F(NodeTest, HasNextInit){
+  ASSERT_FALSE(_obj.hasNext());
+}
+
+TEST_F(NodeTest, HasNextHavingANextObj){
+  Node<int> *o1 = new Node<int>();
+  _obj.setNext(o1);
+  ASSERT_TRUE(_obj.hasNext());
+  delete o1;
 }
  
 int main(int argc, char **argv) {
