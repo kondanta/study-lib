f268476638e9b37a269b5d26fe752e27effe9bda
Taylan Dogan <doganntaylan@gmail.com>
2018-11-29 02:36:46 +0300

    Add loo training and prediction
    Signed-off-by: Taylan Dogan <doganntaylan@gmail.com>

--- Taylan/machine-learning/project/main.py
+++ Taylan/machine-learning/project/main.py
@@ -1,5 +1,5 @@
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import LeaveOneOut, train_test_split
 from sklearn import metrics
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,7 +7,6 @@ import csv
 
 def create_lists(name):
     """Creating the X y values for testing"""
-    zeroes = []
     x1 = []
     x2 = []
     x3 = []
@@ -17,9 +16,8 @@ def create_lists(name):
     with open(name, 'r', newline='', encoding='ISO-8859-1') as csvfile:
         rows = csv.DictReader(csvfile)
         for j, i in enumerate(rows):
-            if j == 100:
+            if j >= 100:
                 break
-            zeroes = np.append(zeroes, 0).astype(dtype=float)
             x1 = np.append(x1, i['x1']).astype(dtype=float)
             x2 = np.append(x2, i['x2']).astype(dtype=float)
             x3 = np.append(x3, i['x3']).astype(dtype=float)
@@ -28,8 +26,66 @@ def create_lists(name):
             y = np.append(y, i['Y']).astype(dtype=float)
         
     csvfile.close()
-    X = np.array([zeroes, x1, x2, x3, x4, x5])
+    X = np.array([x1, x2, x3, x4, x5])
     return X, y
+
+def create_actual_data_lists(name):
+    x1 = []
+    x2 = []
+    x3 = []
+    x4 = []
+    x5 = []
+    with open(name, 'r', newline='', encoding='ISO-8859-1') as csvfile:
+        rows = csv.DictReader(csvfile)
+        for j, i in enumerate(rows):
+            if j >= 100:
+                x1 = np.append(x1, i['x1']).astype(dtype=float)
+                x2 = np.append(x2, i['x2']).astype(dtype=float)
+                x3 = np.append(x3, i['x3']).astype(dtype=float)
+                x4 = np.append(x4, i['x4']).astype(dtype=float)
+                x5 = np.append(x5, i['x5']).astype(dtype=float)
+    csvfile.close()
+    X = np.array([x1,x2,x3,x4,x5])
+    return X
+
+def calculate_error(predictions, y):
+    """
+    Calculating the error margin
+    """
+    error = []
+    for i, pred in enumerate(predictions):
+        error.append(pred - y[i])
+    return error
+
+
+X, y = create_lists("project-data.csv")
+X = np.transpose(X)
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# clf = LinearRegression(n_jobs=-1)
+
+# clf.fit(X_train, y_train)
+# pr = clf.predict(X_test)
+# err = calculate_error(pr, y)
+
+# accuracy = clf.score(X_test, y_test)
+# accr2 = clf.score(X_train, y_train)
+
+# PredListt = create_actual_data_lists("project-data.csv")
+# PredListt = np.transpose(PredListt)
+
+# pred_set = clf.predict(PredListt)
+# pred_err = calculate_error(pred_set, y)
+
+
+# print(pred_set, accuracy, accr2)
+
+# plt.scatter(pr, err, c='b')
+# plt.scatter(pred_set, pred_err, c='r')
+# plt.show()
+
+model = LinearRegression()
 
 def do_calculation():
     X, y = create_lists('project-data.csv')
@@ -43,8 +99,22 @@ def do_calculation():
     print_mse_for_all_data(err)  # just in case
 
     test_pred, test_err = leave_one_out(X, y)
-    plot(predictions, err, test_pred, test_err)
 
+    pred_list = create_actual_data_lists('project-data.csv')
+   
+    pr = predict_values(pred_list, y)
+    print(pr[0])
+    
+    plot(predictions, err, test_pred, test_err)
+    
+
+
+def predict_values(pred_list, y):
+    pred_list = np.transpose(pred_list)
+    pred_set = model.predict(pred_list)
+    pred_err = calculate_error(pred_set, y)
+
+    return pred_set, pred_err
 
 def calculate_error(predictions, y):
     """
@@ -85,12 +155,15 @@ def loo_training(X, y):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
-        model = LinearRegression()
+        #model = LinearRegression()
         model.fit(X=X_train, y=y_train)
         y_pred = model.predict(X_test)
 
         ytests += list(y_test)
         ypredictions += list(y_pred)
+        print("Test confidence: {0}, \
+        Train confidence: {1} " .format(model.score(X_test, y_test),
+                                        model.score(X_train, y_train)))
 
     return ytests, ypredictions
 
@@ -110,4 +183,3 @@ def print_mse_for_all_data(errors):
 
 
 do_calculation()
-
